75c9d64220883450752b867599f6ef153ee0cb8e
Alice Mon <88655866+AliceMonGithub@users.noreply.github.com>
2023-09-26 09:41:13 +0300

    Initial commit

---
+++ Tetris/Tetris.c
@@ -0,0 +1,174 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <raylib.h>
+
+typedef struct Vector2Int
+{
+	int x;
+	int y;
+} Vector2Int;
+
+struct Vector2Int L_BRICK[4] = 
+{
+	{ 0, 0 },
+	{ 1, 0 },
+	{ 2, 0 },
+	{ 2, 1 }
+};
+
+typedef struct Brick
+{
+	Vector2Int cells[4];
+	Vector2Int position;
+} Brick;
+
+typedef struct BricksList
+{
+	Brick* value;
+	int lenght;
+} BricksList;
+
+typedef struct Game
+{
+	BricksList bricks;
+	Brick* controllableBrick;
+
+	float fallTimer;
+} Game;
+
+const float FALL_TIME = 1.3f;
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 800;
+
+const int START_X_POSITION = 5;
+const int START_Y_POSITION = 20;
+
+const int GRID_SIZE_X = 10;
+const int GRID_SIZE_Y = 20;
+
+const int GRID_OFFSET_X = 200;
+const int GRID_OFFSET_Y = 700;
+
+const int CELL_SIZE = 30;
+
+int TrySpawnNext(Game* game)
+{
+	if (game->controllableBrick->position.y == 0)
+	{
+		Brick instance;
+
+		instance.position.x = START_X_POSITION;
+		instance.position.y = START_Y_POSITION;
+
+		memcpy(instance.cells, L_BRICK, sizeof(L_BRICK));
+
+		Brick* ptr = (Brick*)realloc(game->bricks.value, sizeof(Brick) * (game->bricks.lenght + 1));
+
+		if (ptr == NULL)
+		{
+			return;
+		}
+
+		game->bricks.value = ptr;
+		game->bricks.lenght++;
+
+		game->bricks.value[game->bricks.lenght - 1] = instance;
+		game->controllableBrick = &game->bricks.value[game->bricks.lenght - 1];
+	}
+}
+
+int Update(Game* game)
+{
+	game->fallTimer += GetFrameTime();
+
+	if (IsKeyPressed(KEY_D))
+	{
+		game->controllableBrick->position.x++;
+	}
+	else if (IsKeyPressed(KEY_A))
+	{
+		game->controllableBrick->position.x--;
+	}
+	else if (IsKeyPressed(KEY_S))
+	{
+		game->controllableBrick->position.y = 0;
+		game->fallTimer = 0;
+
+		TrySpawnNext(game);
+	}
+
+	if (game->fallTimer >= FALL_TIME)
+	{
+		game->controllableBrick->position.y--;
+		game->fallTimer = 0;
+
+		TrySpawnNext(game);
+	}
+}
+
+int RenderBrick(Brick brick)
+{
+	for (int i = 0; i < sizeof(brick.cells) / sizeof(brick.cells[0]); i++) {
+		Vector2 cellPosition = { 0, 0 };
+
+		cellPosition.x += GRID_OFFSET_X + (CELL_SIZE * brick.position.x) + (CELL_SIZE * brick.cells[i].x);
+		cellPosition.y += GRID_OFFSET_Y - (CELL_SIZE * brick.position.y) - (CELL_SIZE * brick.cells[i].y);
+
+		DrawRectangle(cellPosition.x, cellPosition.y, CELL_SIZE, CELL_SIZE, BLUE);
+	}
+}
+
+int RenderBricks(Game* game)
+{
+	for (int i = 0; i < game->bricks.lenght; i++)
+	{
+		RenderBrick(game->bricks.value[i]);
+	}
+}
+
+int Render(Game* game)
+{
+	RenderBricks(game);
+}
+
+int RunLoop(Game* game)
+{
+	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Tetris");
+	SetTargetFPS(60);
+
+	while (WindowShouldClose() == false)
+	{
+		BeginDrawing();
+
+		ClearBackground(RAYWHITE);
+
+		Update(game);
+		Render(game);
+
+		EndDrawing();
+	}
+
+	CloseWindow();
+}
+
+int main()
+{
+	Vector2Int test = { 0, 0 };
+
+	printf("Hello, World! %d | %d", test.x, test.y);
+
+	Game game = { {(Brick*)malloc(sizeof(Brick)), 1}, NULL, 0};
+
+	game.bricks.value[0].position.x = START_X_POSITION;
+	game.bricks.value[0].position.y = START_Y_POSITION;
+
+	memcpy(game.bricks.value[0].cells, L_BRICK, sizeof(L_BRICK));
+
+	game.controllableBrick = &game.bricks.value[0];
+
+	RunLoop(&game);
+
+	return 0;
+}
